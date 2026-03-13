55dcd07178c6cacd756da4b766f5b55f91a6ac06
Rafhielle Allen Alcabaza <allen2talcabaza@gmail.com>
2026-03-13 23:41:56 +0800

    feats: fixed layout & added logo

--- app/Views/index.php
+++ app/Views/index.php
@@ -11,7 +11,11 @@
 <body>
 
     <nav>
-        <div class="logo">Website-Logo</div>
+        <div class="logo">
+            <a href="<?= base_url() ?>">
+                <img src="<?= base_url('images/Logo.png') ?>" alt="Furniture Brand Logo">
+            </a>
+        </div>
         <div class="nav-links">
             <a href="#">Home</a>
             <a href="#">About</a>
@@ -20,7 +24,10 @@
     </nav>
 
     <header class="hero">
-        <h1>Crafted In Wood. Built to last.</h1>
+        <h1>
+            Crafted In Wood.
+            <span>Built to last.</span>
+        </h1>
         <p>Handmade wooden furniture and pieces shaped by time, grain, and care.</p>
         <a href="#" class="btn">Show more</a>
     </header>
