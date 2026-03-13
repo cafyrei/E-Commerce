af8f303cd56274de1ed83fcb8221bafb80831807
Rafhielle Allen Alcabaza <allen2talcabaza@gmail.com>
2026-03-13 22:18:41 +0800

    Initial commit: CI4 E-Commerce Project Structure

---
+++ app/Views/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('css/main.css') ?>">
+    <title>E-Commerce</title>
+</head>
+
+<body>
+
+    <nav>
+        <div class="logo">Website-Logo</div>
+        <div class="nav-links">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </div>
+    </nav>
+
+    <header class="hero">
+        <h1>Crafted In Wood. Built to last.</h1>
+        <p>Handmade wooden furniture and pieces shaped by time, grain, and care.</p>
+        <a href="#" class="btn">Show more</a>
+    </header>
+
+
+</body>
+
+</html>
